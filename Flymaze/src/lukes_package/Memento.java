5d947e78c6f2743787dc9dadfc61c74e0c938d52
LukeEire <18250785@studentmail.ul.ie>
2022-04-04 00:09:49 +0100

    Basic memento and test player class

---
+++ Flymaze/src/lukes_package/Memento.java
@@ -0,0 +1,22 @@
+package lukes_package;
+
+public class Memento {
+
+	PlayerOLD playerSave;
+	Map mapSave;
+	
+	
+	public void restoreState(PlayerOLD p, Map m) {
+		p = playerSave;
+		m = mapSave;
+	}
+	
+	public void savePLayer(PlayerOLD p) {
+		playerSave = p;
+	}
+	
+	public void saveMap(Map m) {
+		mapSave = m;
+	}
+	
+}
