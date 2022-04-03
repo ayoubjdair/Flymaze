2d44d687db7eaaf93cd566eaf442d42116af14dd
ayoubjdair <ayoub324@icloud.com>
2022-04-04 00:12:53 +0100

    Enemy package fully integrated

---
+++ Flymaze/src/enemy_package/Enemies.java
@@ -0,0 +1,86 @@
+package enemy_package;
+
+// Composite class
+import java.util.ArrayList;
+import java.util.List;
+  
+public class Enemies implements Enemy
+{
+    private List<Enemy> enemiesList = new ArrayList<Enemy>();
+
+    // Getters
+    @Override
+    public int getRoomNumber() {
+        for(Enemy en:enemiesList)
+        {
+            return en.getRoomNumber();
+        }
+        return 0;
+    }
+
+    @Override
+    public int getHealth() 
+    {
+        for(Enemy en:enemiesList)
+        {
+            return en.getHealth();
+        }
+        return 0;
+    }
+
+    @Override
+    public Boolean getIsDead() {
+        for(Enemy en:enemiesList)
+        {
+            return en.getIsDead();
+        }
+        return false;
+    }
+
+    // Setters
+    @Override
+    public void setHealth(int health) 
+    {
+        for(Enemy en:enemiesList)
+        {
+            en.setHealth(health);
+        }
+    }
+
+    @Override
+    public void setRoomNumber(int room) {
+        for(Enemy en:enemiesList)
+        {
+            en.setRoomNumber(room);
+        }
+    }
+
+    @Override
+    public void setIsDead(Boolean dead) {
+        for(Enemy en:enemiesList)
+        {
+            en.setIsDead(dead);;
+        }
+    }
+
+    public String toString(){
+        String desc = "\n ";
+        for(Enemy en:enemiesList)
+        {
+            desc += en.toString();
+        }
+        return desc;
+    }
+
+    public void addEnemy(Enemy en)
+    {
+        enemiesList.add(en);
+    }
+       
+    public void removeEnemy(Enemy en)
+    {
+        enemiesList.remove(en);
+    }
+
+    
+}
---
+++ Flymaze/src/enemy_package/Enemy.java
@@ -0,0 +1,20 @@
+package enemy_package;
+// Demonstrates composite design pattern
+// Operations that can be performed on one enemy can be performed on a group of them
+// package enemies;
+//BOSS Enemy - has other low level enemies below it
+// Interface Componenet
+public interface Enemy {
+
+    // Getters
+    public int getHealth();
+    public int getRoomNumber();
+    public Boolean getIsDead();
+
+    // Setters
+    public void setHealth(int health);
+    public void setRoomNumber(int room);
+    public void setIsDead(Boolean dead);
+
+    public String toString();
+}
