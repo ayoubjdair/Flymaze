9424d622eb3fe947cdfc444764365b6fc04075b4
LukeEire <18250785@studentmail.ul.ie>
2022-04-04 21:59:42 +0100

    Added temp testing class -Luke

---
+++ Flymaze/src/testing_package/tempTestingClass.java
@@ -0,0 +1,23 @@
+package testing_package;
+
+public class tempTestingClass {
+
+	
+	
+	public static void main(String[] args){
+		
+		for (int i = 0; i<20; i++) {
+			int zombieRoom = (int) ((Math.random() * (9 - 1)) + 1);
+			System.out.println("Number is:" + zombieRoom );
+		}
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+}
